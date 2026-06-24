fac1dcf9d846a5177ef771fe9db6371a40aff346
Vitoria Fortes <fortesvitoria@gmail.com>
2026-06-24 11:07:20 -0300

    inclui crud produtos no app.js

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ injetando o decodificador JSON nativo e amarrando o arquivo de rotas globais.*/
 const express = require('express');
 const clienteRouter = require('./router/cliente_router');
 
+const produtoRouter = require('./router/produto_router');
+
 const app = express();
 
 // Middleware obrigatório para permitir o recebimento e leitura de corpos de requisição em formato JSON
 app.use(express.json());
 
-// Injeta o arquivo de rotas do cliente associado ao prefixo global da API (/api)
+// Injeta o arquivo de rotas do cliente e produto associado ao prefixo global da API (/api)
 app.use('/api', clienteRouter);
+app.use('/api', produtoRouter);
+
 
 // Exporta o aplicativo configurado para ser ligado pelo arquivo centralizador server.js
 module.exports = app;
