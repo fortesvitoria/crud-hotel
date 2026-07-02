0967e977e3ef533c81679f12fd90083c23caaf51
Vitoria Fortes <fortesvitoria@gmail.com>
2026-07-02 11:45:26 -0300

    ajusta sql

--- repository/quarto_repository.js
+++ repository/quarto_repository.js
@@ -3,7 +3,7 @@ const pool = require('../database');
 async function listarQuartos() {
     const client = await pool.connect();
     try {
-        const sql = 'SELECT * FROM quartos ORDER BY id ASC';
+        const sql = 'SELECT * FROM quartos ORDER BY numero ASC';
         const result = await client.query(sql);
         return result.rows;
     } finally {
@@ -14,7 +14,7 @@ async function listarQuartos() {
 async function inserirQuarto(quarto) {
     const client = await pool.connect();
     try {
-        const sql = "INSERT INTO quartos (numero, tipo_id, valor_diaria, status) VALUES ($1, $2, $3, $4) RETURNING *";
+        const sql = "INSERT INTO quartos (numero, id_tipo, valor_diaria, status) VALUES ($1, $2, $3, $4) RETURNING *";
         const values = [quarto.numero, quarto.tipo_id, quarto.valor_diaria, quarto.status];
         const result = await client.query(sql, values);
         return result.rows[0];
@@ -26,7 +26,7 @@ async function inserirQuarto(quarto) {
 async function buscarQuartoPorId(id) {
     const client = await pool.connect();
     try {
-        const sql = 'SELECT * FROM quartos WHERE id = $1';
+        const sql = 'SELECT * FROM quartos WHERE numero = $1';
         const values = [id];
         const result = await client.query(sql, values);
         return result.rows[0];
@@ -36,7 +36,7 @@ async function buscarQuartoPorId(id) {
 }
 
 async function atualizarQuarto(id, quarto) {
-    const sql = 'UPDATE quartos SET numero = $1, tipo_id = $2, valor_diaria = $3, status = $4 WHERE id = $5 RETURNING *';
+    const sql = 'UPDATE quartos SET numero = $1, id_tipo = $2, valor_diaria = $3, status = $4 WHERE numero = $5 RETURNING *';
     const values = [quarto.numero, quarto.tipo_id, quarto.valor_diaria, quarto.status, id];
     
     const client = await pool.connect();
@@ -49,7 +49,7 @@ async function atualizarQuarto(id, quarto) {
 }
 
 async function deletarQuarto(id) {
-    const sql = 'DELETE FROM quartos WHERE id = $1 RETURNING *';
+    const sql = 'DELETE FROM quartos WHERE numero = $1 RETURNING *';
     const values = [id];
     
     const client = await pool.connect();
